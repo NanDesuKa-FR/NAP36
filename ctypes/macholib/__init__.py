71129f7140bb9bb3193120f48873afb59a0e8117
techno-arch <52927301+techno-arch@users.noreply.github.com>
2021-08-08 18:21:45 -0500

    Add files via upload

---
+++ ctypes/macholib/__init__.py
@@ -0,0 +1,12 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)]
+# Embedded file name: ctypes\macholib\__init__.py
+"""
+Enough Mach-O to make your head spin.
+
+See the relevant header files in /usr/include/mach-o
+
+And also Apple's documentation.
+"""
+__version__ = '1.0'
---
+++ google/protobuf/pyext/__init__.py
@@ -0,0 +1,8 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.6 (3379)
+# Decompiled from: Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 08:06:12) [MSC v.1900 64 bit (AMD64)]
+# Embedded file name: lib\site-packages\google\protobuf\pyext\__init__.py
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
